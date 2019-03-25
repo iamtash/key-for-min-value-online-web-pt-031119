14a979c61782a218f1bf6be7db8df6d51fb5d109
Natasha Chernis <natashachernis@gmail.com>
2019-03-25 01:22:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |name, value|
+    binding.pry
     if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = name
